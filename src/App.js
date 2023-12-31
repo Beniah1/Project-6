81796ca43d6109eb19d339cc4799f1076fb15204
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2023-12-31 19:38:03 +0000

    Add Bootstrap CSS and JS links

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <main>
+        <section>
+          <div class="container">
+            <div class="row">
+              {/* Main Article */}
+              <article>
+                <div class="col-6">Column1</div>
+              </article>
+              {/* Sidebar or Related Information */}
+              <aside>
+                <div class="col-6">Column2</div>
+              </aside>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
