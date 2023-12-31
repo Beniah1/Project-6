ea531813ce1c141712d5b000bc5048cec129a30b
Beniah Opong Diallo <136281757+Beniah1@users.noreply.github.com>
2023-12-31 20:32:00 +0000

    Refactor App.js and index.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
 import "./App.css";
-
+import image from "./design/bg-img.jpeg";
 function App() {
   return (
     <div>
-      <main>
-        <section>
-          <div class="container">
-            <div class="row">
-              {/* Main Article */}
-              <article>
-                <div class="col-6">Column1</div>
-              </article>
-              {/* Sidebar or Related Information */}
-              <aside>
-                <div class="col-6">Column2</div>
-              </aside>
-            </div>
-          </div>
-        </section>
-      </main>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "100vh" }}
+      >
+        <div className="bg-white shadow rounded-5 pt-3 pb-3 px-2 w-75">
+          <div className=""></div>
+          <main>
+            <section>
+              <div className="container">
+                <div className="row">
+                  {/* Main Article */}
+                  <article className="col-6">
+                    <h1
+                      className="text-center"
+                      style={{
+                        marginTop: "60px",
+                        fontSize: "3.4rem",
+                        fontWeight: "720",
+                      }}
+                    >
+                      {" "}
+                      Stay updated!
+                    </h1>
+                    <p className="mx-5 mt-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit:
+                    </p>
+                    <p className="mx-5">
+                      {/* to add a margin-right in bootstrap use me-4 */}
+                      <i class="bi bi-check-circle-fill me-3"></i>Lorem ipsum
+                      dolor sit amet, consectetur
+                    </p>
+                    <p className="mx-5">
+                      <i class="bi bi-check-circle-fill me-3"></i>
+                      Lorem ipsum dolor sit amet, sucorereng
+                    </p>
+                    <p className="mx-5">
+                      <i class="bi bi-check-circle-fill me-3"></i>
+                      Lorem ipsum dolor sit amet,
+                    </p>
+                    
+                  </article>
+                  {/* Sidebar or Related Information */}
+                  <aside className="col-6">
+                    {/* main img */}
+                    <img src={image} className="img-fluid rounded-5" alt="" />
+                  </aside>
+                </div>
+              </div>
+            </section>
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
